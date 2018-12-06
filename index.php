53605743e9960f23efdfb1dbcd0804004408c3ad
tueseed <tue_seed@hotmail.com>
2018-12-06 13:04:52 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -163,7 +163,7 @@
 			<div class="row">
 				<div class="col-lg-12">
 					<div class="table-responsive">
-						<table class="table table-hover" id="tbl">
+						<table class="table table-hover">
 							<thead class="thead-dark">
 								<tr>
 									<th scope="col">#</th>
@@ -174,7 +174,7 @@
 									
 								</tr>
 							</thead>
-							<tbody>
+							<tbody id="tbl">
 								<tr>
 									<td>dsadsdasd</td>
 								</tr>
