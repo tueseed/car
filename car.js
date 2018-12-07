8153e8f47c30513bbe13ec44fc722fbff7cfbb92
tueseed <tue_seed@hotmail.com>
2018-12-07 13:25:20 +0700

    Add files via upload

--- car.js
+++ car.js
@@ -14,7 +14,6 @@ function car_data()
 						var obj = JSON.parse(response);
                         //alert(obj.car1.car_num);
 						var table = document.getElementById("tbl");
-						var j = 1;
 						for(i in obj)
 						{
 							var row = table.insertRow(i);
@@ -22,13 +21,10 @@ function car_data()
 							var cell2 = row.insertCell(1);
 							var cell3 = row.insertCell(2);
 							var cell4 = row.insertCell(3);
-							var cell5 = row.insertCell(4);
-							cell1.innerHTML = j;
-							cell2.innerHTML = obj[i].car_num;
-							cell3.innerHTML = obj[i].type;
-							cell4.innerHTML = obj[i].brand;
-							cell5.innerHTML = obj[i].fuel;
-							j++;
+							cell1.innerHTML = obj[i].car_num;
+							cell2.innerHTML = obj[i].type;
+							cell3.innerHTML = obj[i].brand;
+							cell4.innerHTML = obj[i].fuel;
 						}
                     },
 			});
