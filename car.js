560afae46e0760af87f156baae03bef78d35aba4
tueseed <tue_seed@hotmail.com>
2018-12-07 21:07:07 +0700

    Add files via upload

--- car.js
+++ car.js
@@ -21,10 +21,12 @@ function car_data()
 							var cell2 = row.insertCell(1);
 							var cell3 = row.insertCell(2);
 							var cell4 = row.insertCell(3);
+							var cell5 = row.insertCell(4);
 							cell1.innerHTML = obj[i].car_num;
-							cell2.innerHTML = obj[i].type;
+							cell2.innerHTML = obj[i].car_type;
 							cell3.innerHTML = obj[i].brand;
 							cell4.innerHTML = obj[i].fuel;
+							cell5.innerHTML = "<button type='submit' class='btn btn-default'>QRCODE</button>";
 						}
                     },
 			});
