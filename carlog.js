725f73178844c08f6ed8f412ca364cf8d7dd39d7
tueseed <tue_seed@hotmail.com>
2018-12-09 22:48:11 +0700

    Add files via upload

--- carlog.js
+++ carlog.js
@@ -5,5 +5,5 @@ function find_car()
 	var url = new URL(url_string);
 	var car_num = url.searchParams.get("car_num");
 	var txt_car_num = document.getElementById("car_num");
-	txt_car_num.v = car_num;
+	txt_car_num.value = car_num;
 }
