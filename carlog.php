d375efd181b22f6236606572ed93309233f798ae
tueseed <tue_seed@hotmail.com>
2018-12-09 22:43:41 +0700

    Add files via upload

--- carlog.php
+++ carlog.php
@@ -13,8 +13,7 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-		<script src="car.js"></script>
-		<script src="qrcode.js"></script>
+		<script src="carlog.js"></script>
 		<style type="text/css">
 			.row-center
 			{
